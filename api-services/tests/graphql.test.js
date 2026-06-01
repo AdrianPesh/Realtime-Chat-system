4ab6806a233d61fe28aac2817421997a26a9f8bc
AdrianPesh <adrian.peshev2003@gmail.com>
2026-06-01 20:09:37 +0300

    Fixed tests

--- api-services/tests/graphql.test.js
+++ api-services/tests/graphql.test.js
@@ -1,14 +1,21 @@
-beforeAll(()=>{
-    process.env.JWT_SECRET = "super-secret";
-});
+
 
 const request = require("supertest");
 
 const {createApp} = require("../app");
 const authHelper = require("../helpers/authHelper");
 let app;
+const prisma = require("../config/prisma");
 beforeAll(async()=>{
  app=await createApp();
+});
+beforeEach(async()=>{
+ await prisma.user.create({
+    data:{
+        username:"admin",
+        password_hash:"hash"
+    }
+ });
 });
 
 describe("Workspace API",()=>{
